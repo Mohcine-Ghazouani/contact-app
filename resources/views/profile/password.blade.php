16950283d5f37b73e9d6bd4b16b5c872fe334748
Mouhcine El Ghazouani <147713761+Mohcine-Ghazouani@users.noreply.github.com>
2024-12-23 22:09:14 +0100

    Refactor password update form with improved structure and error handling

--- resources/views/profile/password.blade.php
+++ resources/views/profile/password.blade.php
@@ -7,14 +7,58 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </head>
-@include('nav')
+@include('layouts.navbar')
 <body class="bg-body-secondary">
     <div class="container border p-3 mt-5 rounded bg-white" style="width: 30%;">
-        <form method="POST" action="{{route('user-password.update')}}">
+
+        <form method="POST" action="{{route('user-password.update')}}" class="mt-6 space-y-6">
+            @csrf
+            @method('put')
+            <div class="mb-4 center">
+                <h1>UPDATE PASSWORD</h1>
+            </div>
+
+            @if (session('status'))
+            <div class="alert alert-success">
+                {{session('status')}}
+            </div>
+            @endif
+
+            
+            <label for="update_password_current_password" class="form-label"  :value="__('Current Password')" >Current Password</label>
+            <input id="update_password_current_password" name="current_password" type="password"  class="form-control" autocomplete="current-password" />
+            <error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+            <br>
+
+            
+            <label for="update_password_password" class="form-label" :value="__('New Password')" >New Password</label>
+            <input id="update_password_password" name="password" type="password"  class="form-control" autocomplete="new-password" />
+            <error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+
+            <br>
+            
+            <label for="update_password_password_confirmation" class="form-label" :value="__('Confirm Password')" >Confirm Password</label>
+            <input id="update_password_password_confirmation" name="password_confirmation" type="password"  class="form-control" autocomplete="new-password" />
+            <error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+            <br>
+
+            <div class="d-grid mu-2">
+                <button class="btn btn-primary bg-primary btn-block">
+                    Update Password
+                </button>
+            </div>
+        </form>
+
+
+
+        <!-- <form method="POST" action="{{route('user-password.update')}}">
             @csrf
             @method('PUT')
-            <div class="mb-4 text-center">
+            <div class="mb-4 center">
                 <h1>UPDATE PASSWORD</h1>
             </div>
 
@@ -25,17 +69,16 @@
             @endif
 
             <label for="current_password" class="form-label">Current Password : </label>
-            <input id="current_password" class="form-control" type="password" name="email" @error('current_password','updatePassword') is-invalid @enderror required autofocus />
+            <input id="current_password" class="form-control" type="password" name="password"  />
             @error('current_password','updatePassword')
-            <div class="text-danger">{{$message}}</div>
+            <div class="danger">{{$message}}</div>
             @enderror
             <br />
 
             <label for="password" class="form-label">New Password:</label>
-            <input id="password" class="form-control" type="password" name="password" @error('password','updatePassword') is-invalid @enderror required
-                autocomplete="new-password" />
+            <input id="password" class="form-control" type="password" name="password" @error('password','updatePassword') @enderror required />
             @error('password','updatePassword')
-            <div class="text-danger">{{$message}}</div>
+            <div class="danger">{{$message}}</div>
             @enderror
             <br />
         
@@ -44,7 +87,7 @@
             <input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required
                 autocomplete="new-password" />
             @error('password_confirmation','updatePassword')
-            <div class="text-danger">{{$message}}</div>
+            <div class="danger">{{$message}}</div>
             @enderror  
             <br />
 
@@ -53,7 +96,8 @@
                     Update Password
                 </button>
             </div>
-        </form>
+
+        </form> -->
     </div>
 </body>
 
