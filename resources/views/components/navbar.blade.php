f31d09acfaf8f1104b81b603300d5166fcf9284e
Mouhcine El Ghazouani <147713761+Mohcine-Ghazouani@users.noreply.github.com>
2024-12-25 09:39:41 +0100

    navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,44 +1,73 @@
-<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
 
-            <a href="/" class="navbar-brand text-info" >Home</a>
-            <a href="/home" class="navbar-brand text-primary">Accueil</a>
-            <a href="/list" class="navbar-brand text-primary">Contacts</a>
-            
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <a href="/" class="navbar-brand text-info" >Home</a>
+    <a href="/home" class="navbar-brand text-primary">Accueil</a>
+    <a href="/list" class="navbar-brand text-primary">Contacts</a>
+           
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                </ul>
-            </div>
+      </ul>
+    </div>
             
-            <form class="d-flex" action="{{route('logout')}}" method="POST">
-            @csrf
-                <nav class="-mx-3 flex flex-1 justify-end">
-                    
-                    <ul class="navbar-nav">
-                        
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {{auth()->user()->name}}
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="{{route('profile.edit')}}">Profile</a></li>
-                                <li><a class="dropdown-item" href="{{route('profile.password')}}">Update Password</a></li>
-                                <li><button type="submit" class="dropdown-item">LogOut</button></li>
-                            </ul>
-                        </li>
-                    </ul>
+    <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+      Link
+    </a>
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <div>
+          Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+        </div>
+        <div class="dropdown mt-3">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+            Dropdown button
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <form class="d-flex" action="{{route('logout')}}" method="POST">
+    @csrf
+      <nav class="-mx-3 flex flex-1 justify-end">
+                   
+        <ul class="navbar-nav">
+                          
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{auth()->user()->name}}
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="{{route('profile.edit')}}">Profile</a></li>
+                <li><a class="dropdown-item" href="{{route('profile.password')}}">Update Password</a></li>
+                <li><button type="submit" class="dropdown-item">LogOut</button></li>
+              </ul>
+          </li>
+        </ul>
                              
 
-                </nav>
+      </nav>
                
-            </form>
-        </div>
+    </form>
+  </div>
 
-</nav> -->
+</nav>
 
-<nav class="navbar bg-body-tertiary">
+<!-- <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
-    <!-- <a class="navbar-brand" href="/">Home</a> -->
+    <a class="navbar-brand" href="/">Home</a>
     <a href="/home" class="navbar-brand text-primary">Accueil</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -68,4 +97,4 @@
       </div>
     </div>
   </div>
-</nav>
+</nav> -->
